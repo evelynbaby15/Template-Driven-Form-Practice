8371fbd7a7d17b51f07c6726ff962e986a04a409
evelynbaby15 <evelyn.b9044015@gmail.com>
2018-07-27 16:23:38 +0800

    Get form submitted data and show them in the same page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent implements OnInit {
   answer = '';
   gender = ['male', 'female'];
 
+  userResponse = {
+    username: '',
+    gender: '',
+    email: '',
+    secretQuestion: '',
+    answer: ''
+  };
+  submitted = false;
+
   ngOnInit(): void {
     console.log('form start state from ngOnit:', this.myForm);
   }
@@ -22,11 +31,35 @@ export class AppComponent implements OnInit {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // setValue 可用在 reset value for form
+    // this.myForm.setValue({
+    //   userData: {
+    //     username: 'eve',
+    //     email: 'eve@cc.bb'
+    //   },
+    //   ssssecret: 'teacher',
+    //   guestionAnswer: 'correct ans',
+    //   gender: 'female'
+    // });
+    this.myForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+        email: 'eve@cht.com.tw'
+      }
+    });
+
   }
 
   onSubmit(form: NgForm) {
+    this.submitted = true;
     console.log('Get submitted form:', form);
     // Note that this.myForm is the same with params pass in this method: form.  
     // console.log('test at view child to get form:', this.myForm);
+
+    this.userResponse.username = this.myForm.value.userData.username;
+    this.userResponse.email = this.myForm.value.userData.email;
+    this.userResponse.gender = this.myForm.value.gender;
+    this.userResponse.secretQuestion = this.myForm.value.ssssecret;
+    this.userResponse.answer = this.myForm.value.guestionAnswer;
   }
 }
