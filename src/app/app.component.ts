5e5d84cd614cdd45fd63224e62fff86aa65bae4b
evelynbaby15 <evelyn.b9044015@gmail.com>
2018-07-27 15:12:28 +0800

    Add many form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  defaultQuestion = 'pet';
+  answer = '';
+  gender = ['male', 'female'];
+
+  ngOnInit(): void {
+    console.log('form start state from ngOnit:', this.myForm);
+  }
+  @ViewChild('f') myForm: NgForm;
+
+  constructor() {
+    console.log('form start state from constructor:', this.myForm);
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('Get submitted form:', form);
+    // Note that this.myForm is the same with params pass in this method: form.  
+    // console.log('test at view child to get form:', this.myForm);
   }
 }
